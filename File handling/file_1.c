cfc2225e38c5b9c042f9c3c4e940219bc3fe5af5
Dhruv <dhruvmalusare13@gmail.com>
2025-04-30 18:45:11 +0530

    Added all C files

--- File handling/file_1.c
+++ File handling/file_1.c
@@ -2,5 +2,14 @@
 void main()
 {
    FILE *fp;
-   fp=fopen(""); 
+   fp=fopen("test.txt","r");
+   if(fp==NULL)
+   {
+    printf("File is not found");
+   } 
+   int i;
+   fscanf(fp,"%d",&i);
+   printf("The number is:%d",i);
+
+   fclose(fp);
 }
